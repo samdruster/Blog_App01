c1fc10755c8e47f67e57340a2e0a9257c1722762
samdruster <samdruster@gmail.com>
2020-12-14 22:44:55 +0530

    init

---
+++ fireBase.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+(function () {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD6gLiUyD7F5YBKpx1l0qMduzZiReYDKqk",
+    authDomain: "login-4a6d2.firebaseapp.com",
+    projectId: "login-4a6d2",
+    storageBucket: "login-4a6d2.appspot.com",
+    messagingSenderId: "822224067865",
+    appId: "1:822224067865:web:a77ed8e75f54cb60303988",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  app_fireBase = firebase;
+})();
